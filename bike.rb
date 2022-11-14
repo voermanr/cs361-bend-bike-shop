12037b22c5a4844103c13781228472a0f0c8ab66
Ross Voerman <voermanr@oregonstate.edu>
2022-11-13 18:53:57 -0800

    added pannier class

--- bike.rb
+++ bike.rb
@@ -1,19 +1,18 @@
 # Bike
 
 class Bike
-
   STANDARD_WEIGHT = 200 # lbs
   MAX_CARGO_ITEMS = 10
 
   attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented: false)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @pannier = Pannier.new(MAX_CARGO_ITEMS)
   end
 
   def rent!
@@ -21,19 +20,27 @@ class Bike
   end
 
   def add_cargo(item)
-    self.cargo_contents << item
+    @pannier.cargo_contents << item
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
-  end
-
-  def pannier_capacity
-    MAX_CARGO_ITEMS
+    @pannier.cargo_contents.remove(item)
   end
 
   def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+    @pannier.pannier_remaining_capacity
+  end
+end
+
+class Pannier
+  attr_accessor :capacity, :cargo_contents
+
+  def initialize(capacity)
+    @capacity = capacity
+    @cargo_contents = []
   end
 
+  def pannier_remaining_capacity
+    capacity - cargo_contents.size
+  end
 end
